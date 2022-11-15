3f274aeda9cdbee5f09ffbbcad2a98eb00f4c398
Prashant Zombade <prashantzombade@gmail.com>
2022-11-15 21:20:21 +0530

    Added Rest Template test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@LocalServerPort
+	private int port;
 
+	@Autowired
+	private HelloController controller;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void contextLoads() throws Exception {
+//		assertThat(controller).isNotNull();
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port,
+				String.class)).contains("Hello World from Spring Boot");
+	}
 }
